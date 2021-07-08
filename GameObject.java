d32d33bf458c4f873d4e9f74e862b9329bcdcaa0
Larry Tingxuan Tingxuan <larrytao100@gmail.com>
2021-07-08 19:29:52 +0000

    testing gameobject.interact

--- GameObject.java
+++ GameObject.java
@@ -1,22 +1,49 @@
-import java.util.HashMap;
+import java.util.*;
+
 
 public class GameObject {
+
+  private GameObject bounty;
   private String name;
+
   public GameObject(String name) {
       this.name = name;
+      bounty = null;
+  }
+
+  public GameObject(String name, GameObject bounty) {
+    this.name = name;
+    this.bounty = bounty;
   }
 
   public GameObject() {
     this.name = "None";
+    this.bounty = null;
   }
 
   public GameObject interact() {
+    Scanner s = new Scanner(System.in);
+    System.out.println("You are now interacting with " + name);
+    boolean interacting = true;
+     System.out.println("Please input a command or a line of dialogue to interact with " + name);
+    String input = s.nextLine();
+    if (input.contains("/take")) {
+      if (bounty != null) {
+        System.out.println("Successfully interacted with " + name + ". It gave you a " + bounty.getName());
+        return bounty;
+      }
+    }
     System.out.println("Successfully interacted with " + name);
+    System.out.println("It didn't give you anything.");
     return new GameObject();
   }
 
   public String getName()
   {
     return name;
+  }
+
+  public String setBounty(GameObject bounty) {
+    this.bounty = bounty;
   }
 }
--- Main.java
+++ Main.java
@@ -6,15 +6,24 @@ public class Main {
     public static void main(String[] args) {
         int currentLocation = 0;
         HashMap<String, Runnable> commands = new HashMap<>();
-        Location[] locations;
+        Location[] locations = new Location[2];
+        // Paris initialization
         GameObject eiffelTower = new GameObject("Eiffel Tower");
         GameObject louvre = new GameObject("Louvre");
+        GameObject coolPainting = new GameObject("Mona Lisa");
+        louvre.setBounty(coolPainting);
         GameObject cathedral = new GameObject("Notre Dame Cathedral");
-        GameObject[] objects = {eiffelTower, louvre, cathedral};
-        Location paris = new Location("Paris", objects);
-        locations = new Location[1];
+        GameObject[] parisObjects = {eiffelTower, louvre, cathedral};
+        Location paris = new Location("Paris", parisObjects);
         locations[0] = paris;
         System.out.println(locations[0].getName());
+        // London initialization
+        GameObject palace = new GameObject("Buckingham Palace");
+        GameObject bigBen = new GameObject("Big Ben");
+        GameObject[] londonObjects = {palace, bigBen};
+        Location london = new Location("London", londonObjects);
+        locations[1] = london;
+        System.out.println(locations[1].getName());
         
         Scanner s = new Scanner(System.in);
         GameWorld w = new GameWorld(0, locations);
@@ -44,9 +53,18 @@ public class Main {
                 {
                   help();
                 }
+                else if(c.equals("/teleport"))
+                {
+                  teleport();
+                }
                 else if(c.equals("/quit"))
+                {
                 //maybe set 'cont' equal to false here?
+                  cont = false;
                   break;
+                }
+
+                  
 
             }
 
@@ -74,6 +92,21 @@ public class Main {
       }
       
       
+    }
+
+    public void teleport(){
+      System.out.println("Choose from the following locations to teleport to:");
+      for (int i = 1; i <= locations.length; i++)
+      {
+        System.out.println( "\t" i + ". " locations[i-1].getName());
+      }
+      System.out.println("Select the number corresponding to the location you desire to teleport to: ");
+      int destination = s.nextInt();
+      currentLocation = destination;
+      try
+      {
+        System.out.println("You have arrived in: " + locations[currentLocation].getName() + ". Try the /list command to see what you can interact with.")
+      } c
     }
 
     public static void save() {
